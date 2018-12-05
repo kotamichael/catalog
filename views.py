07f1dbe3a519a5921d63f86855d30b76048f279e
Dakota Michael <dmichael080993@gmail.com>
2018-12-05 13:19:36 -0500

    Replace print statements with updated Python syntax

--- views.py
+++ views.py
@@ -86,7 +86,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
+        print ("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -109,7 +109,7 @@ def gconnect():
     answer = requests.get(userinfo_url, params=params)
 
     data = answer.json()
-    print data
+    print (data)
 
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
@@ -130,7 +130,7 @@ def gconnect():
     output += ' " style = "width: 300px; height: 300px; \
         border-radius: 150px;"> '
     flash("You are now logged in as %s" % login_session['username'])
-    print "done!"
+    print ("done!")
     return output
 
 
@@ -140,22 +140,22 @@ def gdisconnect():
     # Only disconnect a connected user.
     access_token = login_session.get('access_token')
     if access_token is None:
-        print 'Access token is none'
+        print ('Access token is none')
         response = make_response(json.dumps(
             'Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    print 'In gdisconnect access token is %s', access_token
-    print 'User name is: '
-    print login_session['username']
+    print ('In gdisconnect access token is %s', access_token)
+    print ('User name is: ')
+    print (login_session['username'])
     # Execute HTTP GET request to revoke current token.
     # access_token = credentials.access_token
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' \
         % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print 'result is '
-    print result
+    print ('result is ')
+    print (result)
     if result['status'] == '200':
         # Reset the user's session.
         del login_session['access_token']
