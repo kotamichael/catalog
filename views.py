95f0c37e28d33f68be66a4b6d601f1ca4f3d3cc2
Dakota Michael <dmichael080993@gmail.com>
2018-12-05 13:34:29 -0500

    Fix Port issue *fingers crossed*

--- views.py
+++ views.py
@@ -459,4 +459,5 @@ def createUser(login_session):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
